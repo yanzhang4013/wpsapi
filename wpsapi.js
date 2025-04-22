4bdb5c731e5df466d344baff87b23078c618d0d3
yanzhang4013 <44666387+yanzhang4013@users.noreply.github.com>
2025-04-22 15:30:01 +0800

    Delete wpsapi.js

--- wpsapi.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-  try {
-    const response = await fetch("https://jkapi.com/api/day_60s");
-    const data = await response.text(); // 是纯文本格式
-    res.status(200).send(data);
-  } catch (error) {
-    res.status(500).json({ error: "代理请求失败", detail: error.toString() });
-  }
-}
